686db833af62cda493d52a1e8f817fadadc05034
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-07 23:56:11 +0000

    loadedDie exercise1

---
+++ loadedDie.js
@@ -0,0 +1,15 @@
+function makeLoadedDie() {
+    var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+    var i = -1;
+  
+    return function() {
+        i += 1;
+        return list[i];   
+    }
+  }
+  
+  var rollLoadedDie = makeLoadedDie();
+  
+  console.log(rollLoadedDie());  
+  console.log(rollLoadedDie());  
+  console.log(rollLoadedDie()); 
